91c4949971adc2ede2bee833e4a7858bc0fd31e9
dell <2765667249@qq.com>
2018-06-05 17:04:32 +0800

    aa

--- app/src/main/java/com/example/dell/yikezhong3/ui/tuijian/TuijianFragment.java
+++ app/src/main/java/com/example/dell/yikezhong3/ui/tuijian/TuijianFragment.java
@@ -4,18 +4,44 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.RadioGroup;
 
 import com.example.dell.yikezhong3.R;
+import com.example.dell.yikezhong3.ui.tuijian.guanzhu.GuanZhuFragment;
+import com.example.dell.yikezhong3.ui.tuijian.remen.ReMenFragment;
 
 public class TuijianFragment extends Fragment {
+
+    private RadioGroup rg;
+    private FragmentManager manager;
+    private FrameLayout fr;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tuijian_fragment, null);
+        fr = view.findViewById(R.id.fr);
+        rg = view.findViewById(R.id.rg);
+        manager = getActivity().getSupportFragmentManager();
+        manager.beginTransaction().replace(R.id.frame,new ReMenFragment()).commit();
 
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.rb1:
+                        manager.beginTransaction().replace(R.id.frame,new ReMenFragment()).commit();
+                        break;
+                    case R.id.rb2:
+                        manager.beginTransaction().replace(R.id.frame,new GuanZhuFragment()).commit();
+                        break;
+                }
+            }
+        });
         return view;
     }
 }
