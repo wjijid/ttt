227c79bbeee5b49977edfb01d4dcee284830c40b
GalaxyYS <964095010@qq.com>
2018-06-07 20:07:06 +0800

    AttentionFragment

--- app/src/main/java/com/example/dell/yikezhong3/ui/follow/FollowActivity.java
+++ app/src/main/java/com/example/dell/yikezhong3/ui/follow/FollowActivity.java
@@ -1,10 +1,13 @@
 package com.example.dell.yikezhong3.ui.follow;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.dell.yikezhong3.R;
 import com.example.dell.yikezhong3.base.BaseActivity;
@@ -28,14 +31,18 @@ public class FollowActivity extends BaseActivity<AttentionPresenter> implements 
 
     private RecyclerView  recyclerView;
     private FollowAdapter adapter;
+    private TextView foll_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         initview();
+
+
     }
 
     private void initview() {
+
 
         recyclerView=findViewById(R.id.fmRv);
         List<AttentionBean.DataBean> list = new ArrayList<>();
